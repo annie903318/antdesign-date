eb04b8a5cd2d69c8edf3b0f079ca72d953daa906
annie903318 <annie903318@gmail.com>
2020-07-06 14:25:40 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button, DatePicker, version, message,Alert } from "antd";
+import "antd/dist/antd.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    date: "",
+    setDate: ""
+  }
+  handlechange = (value) => {
+    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    this.setState((setDate)=>({setDate:value}))
+    console.log(value)
+  }
+  render() {
+    const { date, setDate } = this.state;
+    return (
+      <div className="App">
+        <h1>antd version: {version}</h1>
+        <DatePicker />
+        <Button type="primary" style={{ marginLeft: 8 }}>
+          Test Button
+        </Button>
+        <br /><br />
+        <div style={{ width: 400, margin: '100px auto' }}>
+          <DatePicker onChange={this.handlechange} />
+          <div style={{ marginTop: 16 }}>
+            {/* Selected Date: {setDate ? setDate.format('YYYY-MM-DD') : 'None'} */}
+            <Alert message="Selected Date" description={setDate ? setDate.format('YYYY-MM-DD') : 'None'} />
+
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+
 }
-
-export default App;
